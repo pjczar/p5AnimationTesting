45371e8b5b06a5335a3f9b28a1f08312ac53bbb7
Paul Czarnecki JR <120606767+pjczar@users.noreply.github.com>
2023-07-22 16:11:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,12 +3,15 @@ let numCols, numRows; // Number of columns and rows in the grid
 let yOffset = 0; // Y offset for the moving grid
 let speed = 0.1; // Speed of the moving grid
 let grid = []; // 2D array to store characters in each cell
+let characterChangeDelay = 20; // Delay for character changes
+let characterChangeDuration = 30; // Duration of character change color effect
+let characterChangeTimer = 0; // Timer for character changes
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   numCols = ceil(width / gridSize) + 1;
   numRows = ceil(height / gridSize / 2) + 1;
-  colorMode(HSB, 100); // Use HSB color mode for bright and saturated colors
+  colorMode(RGB, 255); // Use RGB color mode
 
   // Initialize the grid with random characters
   for (let y = 0; y < numRows; y++) {
@@ -22,31 +25,24 @@ function setup() {
 function draw() {
   background(0); // Black background
   let cellSize = gridSize - 2; // Adjust the cell size for padding
-  
+
   // Draw the grid in the lower half of the window
   for (let y = 0; y < numRows; y++) {
     for (let x = 0; x < numCols; x++) {
       let x1 = x * gridSize - gridSize / 2;
       let y1 = y * gridSize + yOffset + height / 2;
-      
-      // Fill the cell with bright green
-      fill(80, 100, 100);
-      
-      // Add a slight shadow effect
-      let shadowColor = color(80, 100, 100, 80); // Green with alpha value 80
-      let shadowOffset = 2; // Shadow offset in pixels
-      translate(shadowOffset, shadowOffset); // Translate to create the shadow effect
-      rect(x1, y1, cellSize, cellSize);
-      translate(-shadowOffset, -shadowOffset); // Reset translation
-      
+
+      // Fill the cell with black
+      fill(0);
+
       // Draw the alphanumeric character in the cell
-      fill(0, 100, 100); // Bright green text color
+      fill(0, 255, 0); // Green text color
       textSize(12);
       textAlign(CENTER, CENTER);
       text(grid[y][x], x1 + cellSize / 2, y1 + cellSize / 2);
     }
   }
-  
+
   // Calculate the offset for the moving grid
   yOffset += speed;
   if (yOffset >= gridSize) {
@@ -59,6 +55,22 @@ function draw() {
     grid[0] = [];
     for (let x = 0; x < numCols; x++) {
       grid[0][x] = getRandomCharacter();
+    }
+    characterChangeTimer = characterChangeDelay; // Start the character change timer
+  }
+
+  // Chain reaction of character changes
+  if (characterChangeTimer > 0) {
+    characterChangeTimer--;
+    if (characterChangeTimer === 0) {
+      for (let y = 1; y < numRows; y++) {
+        for (let x = 0; x < numCols; x++) {
+          if (random() < 0.2) { // Random chance for character change
+            grid[y][x] = getRandomCharacter();
+          }
+        }
+      }
+      characterChangeTimer = characterChangeDuration; // Restart the character change timer
     }
   }
 }
