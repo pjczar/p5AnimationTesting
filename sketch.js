545278bd577f6cad844c0b84a5ec818b94a939e5
Paul Czarnecki JR <120606767+pjczar@users.noreply.github.com>
2023-07-22 15:36:04 -0400

    format change

--- sketch.js
+++ sketch.js
@@ -1,18 +1,31 @@
-let squareSize = 100; // Size of the square
+let svgImage; // Declare a variable to store the loaded SVG image
+let canvasWidth, canvasHeight;
+
+function preload() {
+  // Load the SVG file during the preload phase
+  svgImage = loadImage('G.svg');
+}
 
 function setup() {
-  createCanvas(400, 400);
+  canvasWidth = windowWidth; // Set initial canvas width to match window width
+  canvasHeight = windowHeight; // Set initial canvas height to match window height
+  createCanvas(canvasWidth, canvasHeight);
   // Other setup code if needed
 }
 
 function draw() {
   background(255);
   
-  // Draw a square at the center of the canvas
-  fill(255, 0, 0); // Red fill color
-  rectMode(CENTER);
-  rect(width / 2, height / 2, squareSize, squareSize);
+  // Update canvas size every tick to match window size
+  if (canvasWidth !== windowWidth || canvasHeight !== windowHeight) {
+    canvasWidth = windowWidth;
+    canvasHeight = windowHeight;
+    resizeCanvas(canvasWidth, canvasHeight);
+  }
   
-  // Apply animations or transformations to the square if needed
+  // Draw the loaded SVG image at the center of the canvas
+  image(svgImage, width / 2 - svgImage.width / 2, height / 2 - svgImage.height / 2);
+  
+  // Apply animations or transformations to the SVG if needed
   // For example, you can use p5.js functions like translate(), rotate(), scale(), etc.
 }
