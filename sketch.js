6f36939e92409aff7cddf61608375b55f3cf7c0f
Paul Czarnecki JR <120606767+pjczar@users.noreply.github.com>
2023-07-22 15:21:46 -0400

    try adding 1 car

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let svgImage; // Declare a variable to store the loaded SVG image
 function preload() {
   // Load the SVG file during the preload phase
   svgImage = loadImage('images/racecar1.svg');
+  svgImage2 = loadImage('images/racecar2.svg');
 }
 
 function setup() {
@@ -15,6 +16,7 @@ function draw() {
   
   // Draw the loaded SVG image at the center of the canvas
   image(svgImage, width / 2 - svgImage.width / 2, height / 2 - svgImage.height / 2);
+  image(svgImage2, width / 3 - svgImage.width / 3, height / 3 - svgImage.height / 3);
   
   // Apply animations or transformations to the SVG if needed
   // For example, you can use p5.js functions like translate(), rotate(), scale(), etc.
